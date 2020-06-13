298d2a2c120b02a7359a85905d661c0cdd10d2eb
rmalloncej <55815281+rmalloncej@users.noreply.github.com>
2020-06-12 21:55:08 -0400

    Add files via upload

---
+++ final/scripts.js
@@ -0,0 +1,64 @@
+var coll = document.getElementsByClassName("title");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+
+        /*if the max-height is currently 500px then make it 0, else make it 500px. Max-height allows the dropdowns to resize to whatever they need to be instead of always expanding to 500px. If we used ".height = auto;" in the JS then it would just snap to place. As far as I know, there is no easy way to make the transition smooth using auto. Trust me on this one. Or don't. Whatever.*/
+
+        if (content.style.maxHeight === "500px") {
+            content.style.maxHeight = "0";
+        } else {
+            content.style.maxHeight = "500px";
+        }
+    });
+}
+
+
+(function (document) {
+    var div = document.getElementById('container');
+    var icon = document.getElementById('icon');
+    var open = false;
+
+    div.addEventListener('click', function () {
+        if (open) {
+            icon.className = 'fa fa-arrow-down';
+        } else {
+            icon.className = 'fa fa-arrow-down open';
+        }
+
+        open = !open;
+    });
+})(document);
+
+
+$(document).ready(function ($) {
+    $('.card-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [{
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+                    },
+            {
+                breakpoint: 400,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                    }
+                ]
+    });
+});
